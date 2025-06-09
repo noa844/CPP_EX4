cca65ca41374625bc05aef650911a7af55638106
Noa <noa.honigstein@gmail.com>
2025-06-09 11:33:07 +0000

    update

--- MyContainer.hpp
+++ MyContainer.hpp
@@ -6,63 +6,105 @@
 
 namespace container {
 
-template<typename T = int>
-class MyContainer {
-private:
-    std::vector<T> data;
+    template<typename T = int>
+    class MyContainer {
+        private:
+            std::vector<T> data;
 
-public:
-    MyContainer() = default;
+        public:
+            MyContainer() = default;
 
-    ~MyContainer() = default;
+            ~MyContainer() = default;
 
-    
-    void add(const T& value) {
-        data.push_back(value);
-    }
-
-    
-    void remove(const T& value) {
-        bool found = false;
-        auto it = data.begin();
-        for (auto it = data.begin(); it != data.end(); ) {
-            if (*it == value) {
-                it = data.erase(it); 
-                found = true;
-            } else {
-                ++it;
+            class AscendingIterator;
+            
+            void add(const T& value) {
+                data.push_back(value);
             }
-        }        
-        if (!found) {
-            throw std::runtime_error("Element not found in container.");
-        }
-    }
 
- 
-    size_t size() const {
-        return data.size();
-    }
-
-    
-    const std::vector<T>& getData() const {
-        return data;
-    }
-
-   
-    friend std::ostream& operator<<(std::ostream& os, const MyContainer<T>& container) {
-        os << "[ ";
-        for (size_t i = 0; i < container.data.size(); ++i) {
-            os << container.data[i];
-            if (i + 1 < container.data.size()) {
-                os << ", ";
+            
+            void remove(const T& value) {
+                bool found = false;
+                auto it = data.begin();
+                for (auto it = data.begin(); it != data.end(); ) {
+                    if (*it == value) {
+                        it = data.erase(it); 
+                        found = true;
+                    } else {
+                        ++it;
+                    }
+                }        
+                if (!found) {
+                    throw std::runtime_error("Element not found in container.");
+                }
             }
+
+        
+            size_t size() const {
+                return data.size();
+            }
+  
+            const std::vector<T>& getData() const {
+                return data;
+            }
+            
+        friend std::ostream& operator<<(std::ostream& os, const MyContainer<T>& container) {
+            os << "[ ";
+            for (size_t i = 0; i < container.data.size(); ++i) {
+                os << container.data[i];
+                if (i + 1 < container.data.size()) {
+                    os << ", ";
+                }
+            }
+            os << " ]";
+            return os;
         }
-        os << " ]";
-        return os;
+
+
+
+class AscendingIterator {
+    private:
+        std::vector<T> sorted_data;
+        size_t index;
+    
+    public:
+        // Constructor for begin()
+        AscendingIterator(const std::vector<T>& original_data): sorted_data(original_data), index(0) {
+            std::sort(sorted_data.begin(), sorted_data.end());
+        }
+
+        // Constructor for end()
+        AscendingIterator(const std::vector<T>& original_data, size_t end_index): sorted_data(original_data), index(end_index) {
+            std::sort(sorted_data.begin(), sorted_data.end());
+        }
+    
+        const T& operator*() const {
+            return sorted_data.at(index);
+        }
+    
+        AscendingIterator& operator++() {
+            ++index;
+            return *this;
+        }
+    
+        bool operator!=(const AscendingIterator& other) const {
+            return index != other.index || sorted_data != other.sorted_data;
+        }
+    
+        bool operator==(const AscendingIterator& other) const {
+            return !(*this != other);
+        }
+    };
+    
+    AscendingIterator begin_ascending_order() const {
+        return AscendingIterator(data);
     }
     
-
+    AscendingIterator end_ascending_order() const {
+        return AscendingIterator(data, data.size());
+    }
     
-};
+        
+    };
 
 } 
--- test/test.cpp
+++ test/test.cpp
@@ -54,3 +54,39 @@ TEST_CASE("Removing a non-existing string throws") {
     c.add("hello");
     CHECK_THROWS_WITH(c.remove("CS"), "Element not found in container.");
 }
+
+TEST_CASE("AscendingIterator: empty container") {
+    MyContainer<int> c;
+    auto it = c.begin_ascending_order();
+    auto end = c.end_ascending_order();
+    CHECK(it == end); // both should point to the end
+}
+
+
+TEST_CASE("AscendingIterator: multiple elements") {
+    MyContainer<int> c;
+    c.add(5);
+    c.add(1);
+    c.add(8);
+    c.add(3);
+
+    std::vector<int> expected = {1, 3, 5, 8};
+    size_t i = 0;
+    for (auto it = c.begin_ascending_order(); it != c.end_ascending_order(); ++it, ++i) {
+        CHECK(*it == expected[i]);
+    }
+}
+
+TEST_CASE("AscendingIterator: stability after remove") {
+    MyContainer<int> c;
+    c.add(7);
+    c.add(3);
+    c.add(5);
+    c.remove(3); // remove the smallest
+
+    std::vector<int> expected = {5, 7};
+    size_t i = 0;
+    for (auto it = c.begin_ascending_order(); it != c.end_ascending_order(); ++it, ++i) {
+        CHECK(*it == expected[i]);
+    }
+}
